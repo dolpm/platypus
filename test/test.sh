79dade6dcdc4b49cbde6d0058a472c4365467c42
Dylan Maloy <dmaloy97@gmail.com>
2023-02-22 15:06:57 -0500

    brrt

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# TODO: fixed this scuffed way of matching .ppus with .txt
 positive_files=($(ls -d ./test_cases/pos_*.ppus))
 positive_expected_output=($(ls -d ./test_cases/pos_*.out))
-
 
 for i in "${!positive_files[@]}"; do
     output=$(../platypus -a ${positive_files[$i]} | diff - ${positive_expected_output[$i]})
@@ -18,6 +16,7 @@ done
 
 negative_files=($(ls -d ./test_cases/neg_*.ppus))
 negative_expected_output=($(ls -d ./test_cases/neg_*.out))
+
 for i in "${!negative_files[@]}"; do
     output=$(../platypus -a ${negative_files[$i]} 2>&1)
     output=$(echo ${output} | diff - ${negative_expected_output[$i]})
