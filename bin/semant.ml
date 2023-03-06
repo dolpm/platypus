7340e62aee29d97fe76e54dae90b3cc2e9fb9f40
Dylan Maloy <dmaloy97@gmail.com>
2023-03-06 15:18:41 -0500

    welp

--- bin/semant.ml
+++ bin/semant.ml
@@ -115,23 +115,33 @@ let check (_things, pipes) =
     let rec lexical_lifetimes parent block ltid lifetime_map =
       match block with
       | Block body ->
-          let child_ids, lifetime_map_with_children =
+          let ( child_ids,
+                lifetime_map_with_children,
+                shallow_bindings,
+                shallow_rebindings,
+                shallow_pipe_ins ) =
             List.fold_left
-              (fun (c_ids, m) s ->
+              (fun (c_ids, m, bs, rbs, pins) s ->
                 match s with
-                | Block [] -> (c_ids, m)
+                | Block [] -> (c_ids, m, bs, rbs, pins)
                 | Block stmts ->
                     let c_ltid =
                       ltid ^ "." ^ string_of_int (List.length c_ids)
                     in
                     ( c_ltid :: c_ids,
-                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m )
+                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m,
+                      bs,
+                      rbs,
+                      pins )
                 | If (_, Block stmts, Block []) ->
                     let c_ltid =
                       ltid ^ "." ^ string_of_int (List.length c_ids)
                     in
                     ( c_ltid :: c_ids,
-                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m )
+                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m,
+                      bs,
+                      rbs,
+                      pins )
                 | If (_, Block stmts, Block stmts2) ->
                     let c1_ltid =
                       ltid ^ "." ^ string_of_int (List.length c_ids)
@@ -144,46 +154,60 @@ let check (_things, pipes) =
                     in
                     ( c2_ltid :: c1_ltid :: c_ids,
                       lexical_lifetimes (Some ltid) (Block stmts2) c1_ltid
-                        s1_map )
+                        s1_map,
+                      bs,
+                      rbs,
+                      pins )
                 | Loop (_, _, _, _, Block stmts) ->
                     let c_ltid =
                       ltid ^ "." ^ string_of_int (List.length c_ids)
                     in
                     ( c_ltid :: c_ids,
-                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m )
+                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m,
+                      bs,
+                      rbs,
+                      pins )
                 | While (_, Block stmts) ->
                     let c_ltid =
                       ltid ^ "." ^ string_of_int (List.length c_ids)
                     in
                     ( c_ltid :: c_ids,
-                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m )
-                | _ -> (c_ids, m))
-              ([], lifetime_map) body
-          in
-          let shallow_bindings =
-            List.filter_map
-              (fun s ->
-                match s with
-                | Assign (is_mut, typ, name, _) -> Some (is_mut, typ, name)
-                | _ -> None)
-              body
-          in
-          let rebindings =
-            List.filter_map
-              (fun s ->
-                match s with ReAssign (name, _) -> Some name | _ -> None)
-              body
-          in
-          let shallow_pipe_ins =
-            List.filter_map
-              (fun s ->
-                match s with
-                | Expr (PipeIn (n, args)) -> Some (n, args)
-                | _ -> None)
+                      lexical_lifetimes (Some ltid) (Block stmts) c_ltid m,
+                      bs,
+                      rbs,
+                      pins )
+                | Assign (is_mut, typ, name, _) ->
+                    let binding =
+                      match c_ids with
+                      | [] -> ((is_mut, typ, name), None)
+                      | last_cid :: _ -> ((is_mut, typ, name), Some last_cid)
+                    in
+                    (c_ids, m, binding :: bs, rbs, pins)
+                | ReAssign (name, _) ->
+                    let rebinding =
+                      match c_ids with
+                      | [] -> (name, None)
+                      | last_cid :: _ -> (name, Some last_cid)
+                    in
+                    (c_ids, m, bs, rebinding :: rbs, pins)
+                | Expr (PipeIn (n, args)) ->
+                    let pin =
+                      match c_ids with
+                      | [] -> ((n, args), None)
+                      | last_cid :: _ -> ((n, args), Some last_cid)
+                    in
+                    (c_ids, m, bs, rbs, pin :: pins)
+                | _ -> (c_ids, m, bs, rbs, pins))
+              ([], lifetime_map, [], [], [])
               body
           in
           StringMap.add ltid
-            (parent, child_ids, shallow_bindings, rebindings, shallow_pipe_ins)
+            ( ltid,
+              parent,
+              child_ids,
+              shallow_bindings,
+              shallow_rebindings,
+              shallow_pipe_ins )
             lifetime_map_with_children
       | _ -> lifetime_map
     in
@@ -221,7 +245,7 @@ let check (_things, pipes) =
         let _ = print_string "\n" in
         let _ =
           StringMap.iter
-            (fun lt (parent, c_ltids, bs, rbs, pipe_ins) ->
+            (fun lt (_ltid, parent, c_ltids, bs, rbs, pipe_ins) ->
               print_string
                 (lt ^ " --> " ^ "\nparent: "
                 ^ (match parent with Some p -> p | None -> "None")
@@ -231,19 +255,32 @@ let check (_things, pipes) =
                     (List.map
                        (fun b ->
                          match b with
-                         | is_mut, typ, name ->
+                         | (is_mut, typ, name), last_lt -> (
                              "(mutable: " ^ string_of_bool is_mut ^ ", type: "
-                             ^ string_of_typ typ ^ ", name: " ^ name ^ ")")
+                             ^ string_of_typ typ ^ ", name: " ^ name
+                             ^ ", last_entered_lt: "
+                             ^
+                             match last_lt with
+                             | None -> "None"
+                             | Some x -> x ^ ")"))
                        bs)
-                ^ "];\nre-binds: [" ^ String.concat "," rbs
+                ^ "];\nre-binds: ["
+                ^ String.concat ","
+                    (List.map
+                       (fun (n, last_lt) ->
+                         "(" ^ "name: " ^ n ^ ", last_entered_lt: "
+                         ^
+                         match last_lt with None -> "None" | Some x -> x ^ ")")
+                       rbs)
                 ^ "];\nshallow pipe-ins: ["
                 ^ String.concat ","
                     (List.map
-                       (fun (n, args) ->
+                       (fun ((n, args), last_lt) ->
                          n ^ "("
                          ^ String.concat ","
                              (List.map (fun a -> string_of_expr a) args)
-                         ^ ")")
+                         ^ ") - last_entered_lt: "
+                         ^ match last_lt with None -> "None" | Some x -> x)
                        pipe_ins)
                 ^ "]\n\n"))
             ltg
@@ -252,4 +289,108 @@ let check (_things, pipes) =
       pipe_lifetimes
   in
 
+  let rec _lifetime_of_binding p_name (ltid, parent, c_ltids, bs, rbs, pipe_ins)
+      var_name =
+    match List.find_opt (fun b -> b = var_name) bs with
+    | None -> (
+        match parent with
+        | None -> None
+        | Some parent ->
+            _lifetime_of_binding parent
+              (StringMap.find parent (StringMap.find p_name pipe_lifetimes))
+              var_name)
+    | Some _ -> Some (ltid, parent, c_ltids, bs, rbs, pipe_ins)
+  in
+
+  let rec _is_borrowed p_name mut (_ltid, _parent, c_ltids, bs, _rbs, _pipe_ins)
+      var_name =
+    match
+      List.find_opt
+        (fun ((_is_mut, typ, v_name), _last_lt) ->
+          if v_name <> var_name then false
+          else
+            match typ with Borrow _ -> !mut | MutBorrow _ -> true | _ -> false)
+        bs
+    with
+    | Some _ -> true
+    | None -> (
+        match
+          List.find_opt
+            (fun c_ltid ->
+              _is_borrowed p_name mut
+                (StringMap.find c_ltid (StringMap.find p_name pipe_lifetimes))
+                var_name)
+            c_ltids
+        with
+        | Some _ -> true
+        | None -> false)
+  in
+
+  let preceeding_blocks p_name root stop =
+    let rec inner (ltid, _parent, c_ltids, _bs, _rbs, _pipe_ins)
+        (ltid', _parent', _c_ltids', _bs', _rbs', _pipe_ins') soln =
+      if ltid = ltid' then (true, soln)
+      else
+        let d, a =
+          List.fold_left
+            (fun (fin, accum) c_ltid ->
+              if fin then (fin, accum)
+              else
+                let c =
+                  StringMap.find c_ltid (StringMap.find p_name pipe_lifetimes)
+                in
+                let f, s = inner c stop [] in
+                (f, s @ accum))
+            (false, []) c_ltids
+        in
+        (d, ltid :: a)
+    in
+    snd (inner root stop [])
+  in
+
+  let _ =
+    let blocks =
+      preceeding_blocks "main"
+        (StringMap.find "main" (StringMap.find "main" pipe_lifetimes))
+        (StringMap.find "main.0.2.0" (StringMap.find "main" pipe_lifetimes))
+    in
+    List.iter (fun s -> print_string (s ^ "\n")) blocks
+  in
+
+  (* takes in a var name and borrow type, returns bool *)
+  (*
+  let _can_be_borrowed p_name (ltid, parent, c_ltids, bs, rbs, pipe_ins)
+      var_name mut =
+    if mut then
+      match
+        lifetime_of_binding p_name
+          (ltid, parent, c_ltids, bs, rbs, pipe_ins)
+          var_name
+      with
+      | None -> false
+      (* must have next node in path to cur to know what is to the LEFT *)
+      | Some (ltid', parent', c_ltids', bs', rbs', pipe_ins') ->
+          (* run a pre-order on the tree with this as our root *)
+          (* stop at ltid *)
+          true
+    else false
+  in
+  *)
+
+  (* variables can be safely immutibly borrowed iff *)
+  (* 1. there are no active mutable borrows on said value *)
+  (* 2. the value is in scope *)
+
+  (* in the context of our lifetime tree, this translates to... *)
+  (* no nodes to the left of current node (under closest re-assignment) *)
+  (* mutably borrow the variable. also, there must not be any reassigments *)
+  (* to the left of our current node. *)
+
+  (* variables can be safely mutably borrowed iff *)
+  (* 1. there are no active borrows (immutable/mutable) on said value *)
+
+  (* in the context of our lifetime tree, this translates to... *)
+  (* no nodes to the left of current node (under closest re-assignment) *)
+  (* borrow the variable. also, there must not be any reassigments *)
+  (* to the left of our current node. *)
   ([], [])
