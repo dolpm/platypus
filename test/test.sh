3384fde4ac8aa4d79f129818fcff615f2d3ec81b
Dylan Maloy <dmaloy97@gmail.com>
2023-03-21 15:23:27 -0400

    tests

--- test/test.sh
+++ test/test.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 
-positive_files=($(ls -d ./test_cases/pos_*.ppus))
-positive_expected_output=($(ls -d ./test_cases/pos_*.out))
+test_dirs=("ast" "sast")
+test_flags=("-a" "-s")
 
-negative_files=($(ls -d ./test_cases/neg_*.ppus))
-negative_expected_output=($(ls -d ./test_cases/neg_*.out))
+for test in "${!test_dirs[@]}"; do
+    test_dir=${test_dirs[$test]}
+    test_flag=${test_flags[$test]}
 
-num_tests=$(( ${#positive_files[@]} + ${#negative_files[@]} ))
-echo "Running $num_tests tests..."
+    
+    positive_files=($(ls -d ./test_cases/${test_dir}/pos_*.ppus))
+    positive_expected_output=($(ls -d ./test_cases/${test_dir}/pos_*.out))
 
-#${#a[@]}
+    negative_files=($(ls -d ./test_cases/${test_dir}/neg_*.ppus))
+    negative_expected_output=($(ls -d ./test_cases/${test_dir}/neg_*.out))
 
-for i in "${!positive_files[@]}"; do
-    output=$(../platypus -a ${positive_files[$i]} | diff - ${positive_expected_output[$i]})
+    num_tests=$(( ${#positive_files[@]} + ${#negative_files[@]} ))
+    echo "Running $num_tests ${test_dir} tests..."
 
-    if [ "$output" != "" ]; then
-        echo "Test ${positive_files[$i]} failed..."
-    else
-        echo "Test ${positive_files[$i]} passed!"
-    fi
+    #${#a[@]}
 
-done
+    for i in "${!positive_files[@]}"; do
+        output=$(../platypus ${test_flag} ${positive_files[$i]} | diff - ${positive_expected_output[$i]})
 
-for i in "${!negative_files[@]}"; do
-    output=$(../platypus -a ${negative_files[$i]} 2>&1)
-    output=$(echo ${output} | diff - ${negative_expected_output[$i]})
+        if [ "$output" != "" ]; then
+            echo "Test ${positive_files[$i]} failed..."
+        else
+            echo "Test ${positive_files[$i]} passed!"
+        fi
 
-    # We want negative tests to throw errors
-    if [ "$output" != "" ]; then
-        echo "Test ${negative_files[$i]} failed..."
-    else
-        echo "Test ${negative_files[$i]} passed!"
-    fi
+    done
+
+    for i in "${!negative_files[@]}"; do
+        output=$(../platypus ${test_flag} ${negative_files[$i]} 2>&1)
+        output=$(echo ${output} | diff - ${negative_expected_output[$i]})
+
+        # We want negative tests to throw errors
+        if [ "$output" != "" ]; then
+            echo "Test ${negative_files[$i]} failed..."
+        else
+            echo "Test ${negative_files[$i]} passed!"
+        fi
+
+    done
 
 done
