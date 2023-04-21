efa67f5417475c9f01a89958585d3ac312e2e5b4
Dylan Maloy <dmaloy97@gmail.com>
2023-04-21 11:27:15 -0400

    add example

---
+++ examples/safety/dangling_ref.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int *talk_to_saul(int *goodmans_cut, int *legal_fees) {
+  if (*goodmans_cut > *legal_fees) {
+    cout << "JESSE! We don't have enough to cook!" << endl;
+    return goodmans_cut;
+  }
+  cout << "Guy's gotta eat... sorry fellas" << endl;
+  return legal_fees;
+}
+
+int *heisenberg(int *methlymine, bool *ricin, int *pseudoephedrine) {
+  int desired_quantity_ml = 1500;
+  int goodmans_cut = (desired_quantity_ml / 20) * 1000;
+  if (*methlymine + *pseudoephedrine < desired_quantity_ml) {
+    int *gc = &goodmans_cut;
+    int *tts = talk_to_saul(gc, methlymine);
+
+    cout << "JESSE! We don't have enough to cook!" << endl;
+    return tts;
+  } else {
+    cout << "Let's cook Jesse." << endl;
+    if (*ricin) {
+      return methlymine;
+    }
+    return pseudoephedrine;
+  }
+}
+
+int main() {
+  int madrigal_supplies = 1000;
+  {
+    int *train_heist = &madrigal_supplies;
+    int pseudo_needed = 100;
+    {
+      int *from_the_hardware_store = &pseudo_needed;
+      bool tuco_angry = true;
+      int *blue_sky =
+          heisenberg(train_heist, &tuco_angry, from_the_hardware_store);
+      cout << *blue_sky << endl;
+    }
+  }
+  return 0;
+}
