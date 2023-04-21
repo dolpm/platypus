6f96f442b0c837a263d0e0c7e915705ab273e22d
Dylan Maloy <dmaloy97@gmail.com>
2023-04-21 11:02:42 -0400

    add supporting comment

--- bin/borrow.ml
+++ bin/borrow.ml
@@ -952,7 +952,8 @@ let borrow_ck pipes built_in_pipe_decls verbose =
                           match a with
                           | _, SIdent n ->
                               if List.mem i idxs then
-                                (* check if is an arg (i.e., will outlive pipe ) *)
+                                (* check if lt is local tp the function *)
+                                (* (i.e., will outlive pipe )... *)
                                 (* throw if not the case *)
                                 let _, lt, _ =
                                   StringMap.find n !ident_assoc_lt_map
